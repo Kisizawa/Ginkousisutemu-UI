72f93d92660b7498d38da789e628d6b61a912aab
Kisizawa <112299224+Kisizawa@users.noreply.github.com>
2022-09-24 20:24:14 +0900

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,18 @@
+import java.util.Objects;
+
+public abstract class Account {
+
+	public void Start() {
+		
+	}
+	public static int next(int a) {
+		// TODO 自動生成されたメソッド・スタブ
+		if(a==1) {
+			AccountNumber.next(a);
+		}
+		return 0;
+	}
+	
+	
+
+}
