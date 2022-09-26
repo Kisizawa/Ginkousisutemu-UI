8565e256d74bcb3b8472778650ca2279ec3b9d05
Kisizawa <112299224+Kisizawa@users.noreply.github.com>
2022-09-26 22:59:59 +0900

    Create AccountMoney.java

---
+++ AccountMoney.java
@@ -0,0 +1,18 @@
+
+public class AccountMoney extends Account{
+	static int Money=0;
+
+	public static int getMoney() {
+		return Money;
+	}
+
+	public void setMoney(int money) {
+		
+		Money = money;
+	}
+	
+	
+	
+	
+
+}
